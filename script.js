e41bdc788f8423c5d53db596ab140af67f7b161b
Samm Wiltshire <samm94tw@gmail.com>
2024-08-15 10:54:57 +0100

    drumkit done

--- script.js
+++ script.js
@@ -1,18 +1,107 @@
-const playclapButton = () => {
-    const audio= document.getElementById("clapSound");
+document.getElementById('boomButton').addEventListener('click', () => {
+    let audio = document.getElementById('boomSound');
     audio.play();
-}
+});
+ 
+document.addEventListener('keydown', (event) => {
+    let audio = document.getElementById('boomSound');
+    if (event.code === 'KeyQ') {
+        audio.play();
+    }
+});
 
- document.addEventListener("clapButton", function(event) {
-     let audio = document.getElementById("clapSound");
-     if (event.code === "KeyA") {
-         audio.play();
-     }
- })
+document.getElementById('clapButton').addEventListener('click', () => {
+    let audio = document.getElementById('clapSound');
+    audio.play();
+});
+ 
+document.addEventListener('keydown', (event) => {
+    let audio = document.getElementById('clapSound');
+    if (event.code === 'KeyW') {
+        audio.play();
+    }
+});
 
-//  document.getElementById("keydown").addEventListener("click", playclapButton);
-//  document.addEventListener("keyPress", (event)=>{
-//      if (event.key==="a"){
-//          playclapButton();
-//     }
-// })
+document.getElementById('hihatButton').addEventListener('click', () => {
+    let audio = document.getElementById('hihatSound');
+    audio.play();
+});
+ 
+document.addEventListener('keydown', (event) => {
+    let audio = document.getElementById('hihatSound');
+    if (event.code === 'KeyE') {
+        audio.play();
+    }
+});
+
+document.getElementById('kickButton').addEventListener('click', () => {
+    let audio = document.getElementById('kickSound');
+    audio.play();
+});
+ 
+document.addEventListener('keydown', (event) => {
+    let audio = document.getElementById('kickSound');
+    if (event.code === 'KeyR') {
+        audio.play();
+    }
+});
+
+document.getElementById('openhatButton').addEventListener('click', () => {
+    let audio = document.getElementById('openhatSound');
+    audio.play();
+});
+ 
+document.addEventListener('keydown', (event) => {
+    let audio = document.getElementById('openhatSound');
+    if (event.code === 'KeyT') {
+        audio.play();
+    }
+});
+
+document.getElementById('rideButton').addEventListener('click', () => {
+    let audio = document.getElementById('rideSound');
+    audio.play();
+});
+ 
+document.addEventListener('keydown', (event) => {
+    let audio = document.getElementById('rideSound');
+    if (event.code === 'KeyY') {
+        audio.play();
+    }
+});
+
+document.getElementById('snareButton').addEventListener('click', () => {
+    let audio = document.getElementById('snareSound');
+    audio.play();
+});
+ 
+document.addEventListener('keydown', (event) => {
+    let audio = document.getElementById('snareSound');
+    if (event.code === 'KeyU') {
+        audio.play();
+    }
+});
+
+document.getElementById('tinkButton').addEventListener('click', () => {
+    let audio = document.getElementById('tinkSound');
+    audio.play();
+});
+ 
+document.addEventListener('keydown', (event) => {
+    let audio = document.getElementById('tinkSound');
+    if (event.code === 'KeyI') {
+        audio.play();
+    }
+});
+
+document.getElementById('tomButton').addEventListener('click', () => {
+    let audio = document.getElementById('tomSound');
+    audio.play();
+});
+ 
+document.addEventListener('keydown', (event) => {
+    let audio = document.getElementById('tomSound');
+    if (event.code === 'KeyO') {
+        audio.play();
+    }
+});
